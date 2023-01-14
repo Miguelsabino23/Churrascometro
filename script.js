3bd46aaf27d8e4d0f522924d55e79a4b9983daa8
miguel sabino <miguelsabino.office@gmail.com>
2023-01-14 20:06:26 -0300

    novas alterações

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
+//Quantidade de pessoas e duração
 let inputAdultos = document.getElementById("adultos");
 let inputCriancas = document.getElementById("criancas");
 let inputDuracao = document.getElementById("duracao");
 
+//Resultado
 let resultado = document.getElementById("resultado")
 
-function calcular() {
 
+
+
+function calcular() {
+    //Pessoas e duração
     let adultos = inputAdultos.value;
     let criancas = inputCriancas.value;
     let duracao = inputDuracao.value;
 
-
+    //Variaveis de armazenamento
     let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
     let qdtTotalCerveja = cervejaPP(duracao) * adultos;
     let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
-    
-    resultado.innerHTML = `<p>${qdtTotalCarne / 1000} Kg de carne</p>`
-    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja / 1000)} Litrão de cerveja</p>`
-    resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebidas / 2000)} Garrafas de refrigerante de 2 litros</p>`
-}
 
-function carnePP(duracao) {
-    if (duracao >= 6) {
-       return 650;
-}   else {
-    return 400;
-  }
-}
-
-function cervejaPP(duracao) {
-    if (duracao >=6) {
-        return 2000; 
+    if (adultos && duracao > 0) {
+        resultado.innerHTML = `<p>${qdtTotalCarne / 1000} Kg de carne</p>`
+        resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja / 1000)} Litrões de cerveja</p>`
+        resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebidas / 2000)} Garrafas de refrigerante de 2 litros</p>`
     } else {
-        return 1200;
+        resultado.innerHTML = `<p>Os valores adultos e duração precisam ser maiores que 0</p>`
     }
 }
 
-function bebidasPP(duracao) {
-    if (duracao >=6) {
-        return 1500;
-    }  else {
-    return 1000;
-  }   
+function carnePP(duracao) {
+    return duracao >= 6 ? 650 : 400;
 }
+
+function cervejaPP(duracao) {
+    return duracao >= 6 ? 2000 : 1200;
+}
+
+
+function bebidasPP(duracao) {
+    return duracao >= 6 ? 1500 : 1000
+}   
